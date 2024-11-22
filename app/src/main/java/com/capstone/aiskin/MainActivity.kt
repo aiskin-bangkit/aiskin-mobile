129d12fc7e2c57dd5bc380b07556517a38113147
Ammar Asysyakur <asysyakurammar125@gmail.com>
2024-11-22 22:55:20 +0700

    MDT 4.3 feat : home fragment

--- app/src/main/java/com/capstone/aiskin/MainActivity.kt
+++ app/src/main/java/com/capstone/aiskin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.capstone.aiskin
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,10 +16,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        supportActionBar?.hide()
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
