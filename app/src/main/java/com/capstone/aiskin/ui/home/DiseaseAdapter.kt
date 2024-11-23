9797d466342dcbc1eef362c0b5abfb37ee440f06
Ammar Asysyakur <asysyakurammar125@gmail.com>
2024-11-24 00:01:04 +0700

    MDT 4.3 feat : home fragment
    recyler view with dummy data

---
+++ app/src/main/java/com/capstone/aiskin/ui/home/DiseaseAdapter.kt
@@ -0,0 +1,38 @@
+package com.capstone.aiskin.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.capstone.aiskin.databinding.ItemDiseaseBinding
+
+data class DiseaseItem(val imageUrl: String, val name: String, val description: String)
+
+class DiseaseAdapter(private val diseaseList: List<DiseaseItem>) :
+    RecyclerView.Adapter<DiseaseAdapter.DiseaseViewHolder>() {
+
+    inner class DiseaseViewHolder(private val binding: ItemDiseaseBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(disease: DiseaseItem) {
+            with(binding) {
+                tvItemName.text = disease.name
+                tvItemDescription.text = disease.description
+                Glide.with(imgItemPhoto.context)
+                    .load("https://www.wowkeren.com/display/images/photo/2023/04/10/00476771.jpg")
+                    .into(imgItemPhoto)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiseaseViewHolder {
+        val binding =
+            ItemDiseaseBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DiseaseViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DiseaseViewHolder, position: Int) {
+        holder.bind(diseaseList[position])
+    }
+
+    override fun getItemCount(): Int = diseaseList.size
+}
--- app/src/main/java/com/capstone/aiskin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/capstone/aiskin/ui/home/HomeFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.capstone.aiskin.core.helper.VerticalItemDecoration
+import com.capstone.aiskin.core.helper.toPx
 import com.capstone.aiskin.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,11 +21,111 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
+        val view = binding.root
+
+        // Setup RecyclerView Disease
+        val diseaseAdapter = DiseaseAdapter(getDummyDiseases())
+        binding.rvSkinDisease.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = diseaseAdapter
+            setHasFixedSize(true)
+        }
+
+    // Set up RecyclerView for Articles
+        val articleAdapter = ArticleAdapter(getDummyArticles())
+        binding.rvNewArticle.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = articleAdapter
+            setHasFixedSize(true)
+        }
+
+
+        return view
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun getDummyDiseases(): List<DiseaseItem> {
+        return listOf(
+            DiseaseItem(
+                "https://www.yalemedicine.org/conditions/psoriasis",
+                "Psoriasis",
+                "Kondisi kulit kronis dengan bercak merah bersisik."
+            ),
+            DiseaseItem(
+                "https://www.honestdocs.id/apa-bedanya-eksim-dan-infeksi-kulit",
+                "Eksim",
+                "Iritasi kulit menyebabkan kemerahan dan gatal."
+            ),
+            DiseaseItem(
+                "https://www.sidomunculstore.com/blog/post/cara-menghilangkan-jerawat-di-pipi-penyebab-solusi-dan-tips.html",
+                "Jerawat",
+                "Penyumbatan pori-pori menyebabkan bintik kecil."
+            ),
+            DiseaseItem(
+                "https://www.halodoc.com/kesehatan/kurap",
+                "Kurap",
+                "Infeksi jamur berbentuk cincin merah di kulit."
+            ),
+            DiseaseItem(
+                "https://www.associatedskincare.com/blog/why-do-i-have-rosacea",
+                "Rosacea",
+                "Kemerahan kronis di wajah dengan bintil kecil."
+            ),
+            DiseaseItem(
+                "https://www.mitrakeluarga.com/artikel/gejala-herpes-zoster",
+                "Herpes Zoster",
+                "Ruam menyakitkan disebabkan oleh virus cacar air."
+            ),
+            DiseaseItem(
+                "https://primayahospital.com/kulit-dan-kelamin/melasma/",
+                "Melasma",
+                "Bercak coklat pada wajah karena paparan matahari."
+            ),
+            DiseaseItem(
+                "https://id.wikipedia.org/wiki/Urtikaria",
+                "Urtikaria",
+                "Biduran dengan bercak merah dan gatal."
+            ),
+            DiseaseItem(
+                "https://www.alomedika.com/penyakit/dermatovenereologi/skabies",
+                "Skabies",
+                "Infeksi kutu menyebabkan rasa gatal di malam hari."
+            ),
+            DiseaseItem(
+                "https://www.google.com/imgres?q=vitiligo&imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F7%2F75%2FVitiligo2.JPG&imgrefurl=https%3A%2F%2Fid.wikipedia.org%2Fwiki%2FVitiligo&docid=TQImtyDJ3RGXbM&tbnid=PmcT5IoQjUUr1M&vet=12ahUKEwjd4K6r4PKJAxV1xjgGHVqMMqEQM3oECB0QAA..i&w=4100&h=2910&hcb=2&ved=2ahUKEwjd4K6r4PKJAxV1xjgGHVqMMqEQM3oECB0QAA",
+                "Vitiligo",
+                "Hilangnya pigmen kulit menyebabkan bercak putih."
+            )
+        )
+    }
+
+    private fun getDummyArticles(): List<ArticleItem> {
+        return listOf(
+            ArticleItem(
+                "https://via.placeholder.com/150",
+                "How to Care for Your Skin",
+                "Learn the best tips to keep your skin healthy and glowing.",
+                "22 Nov 2024"
+            ),
+            ArticleItem(
+                "https://via.placeholder.com/150",
+                "Understanding Skin Conditions",
+                "An overview of common skin conditions and their treatments.",
+                "21 Nov 2024"
+            ),
+            ArticleItem(
+                "https://via.placeholder.com/150",
+                "Top Skincare Myths Debunked",
+                "Discover the truth about skincare myths you may have believed.",
+                "20 Nov 2024"
+            )
+        )
+    }
+
+
 }
